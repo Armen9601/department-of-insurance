46dc8cd0b47a7f59bc316b91f41186238992ba2b
Armen Simonyan <simonyanarmen9601@gmail.com>
2023-03-20 13:11:02 +0400

    some changes for aws

--- 3rd-party-agent/src/main/java/com/example/thirdpartyagent/model/Report.java
+++ 3rd-party-agent/src/main/java/com/example/thirdpartyagent/model/Report.java
@@ -3,22 +3,17 @@ package com.example.thirdpartyagent.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.couchbase.core.mapping.Document;
-import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
 
 import java.util.UUID;
-
-import static org.springframework.data.couchbase.core.mapping.id.GenerationStrategy.UNIQUE;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
+//@Document
 public class Report {
 
-    @Id
-    @GeneratedValue(strategy = UNIQUE)
+    //    @Id
+//    @GeneratedValue(strategy = UNIQUE)
     private UUID id;
     private UUID createdFrom;
     private String agentName;
--- 3rd-party-agent/src/main/java/com/example/thirdpartyagent/repo/ReportRepository.java
+++ 3rd-party-agent/src/main/java/com/example/thirdpartyagent/repo/ReportRepository.java
@@ -1,12 +1,12 @@
-package com.example.thirdpartyagent.repo;
-
-import com.example.thirdpartyagent.model.Report;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-
-@Repository
-public interface ReportRepository extends CouchbaseRepository<Report, UUID> {
-
-}
+//package com.example.thirdpartyagent.repo;
+//
+//import com.example.thirdpartyagent.model.Report;
+//import org.springframework.data.couchbase.repository.CouchbaseRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.UUID;
+//
+//@Repository
+//public interface ReportRepository extends CouchbaseRepository<Report, UUID> {
+//
+//}
